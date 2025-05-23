727c2b8864e86cf89f08efebda607b65b45511e7
ycz <yczaubhreydy@gmail.com>
2025-05-23 18:18:41 +0800

    title definition

--- quiz_text_file_creator.py
+++ quiz_text_file_creator.py
@@ -1,10 +1,20 @@
 # 1. Create a class for the functions. 
 class QuizCreator:
+
 # 2. Define the variables that we are going to use.
     def __init__(self):
         self.title = ""
         self.item_quantity = 0   #These are the relevant datas in my previous code. 
         self.quiz_data = []
-# 3. Define the questions and answers input.
-# 4. Define the file it is going to be saved as.
-# 5. Make a variable storage for the class.
+
+# 3. Define the initial inputs.
+    def get_title(self):
+        self.title = input("What is the title of this quiz?: ").title()    
+
+
+# 4. Define the question and answer inputs.
+
+# 5. Define the file it is going to be saved as.
+
+
+# 6. Make a variable storage for the class.
