154676f445edf5f0d0e1efce5e43964ef9adb398
ycz <yczaubhreydy@gmail.com>
2025-05-23 18:06:44 +0800

    pseudocodes

---
+++ quiz_text_file_creator.py
@@ -0,0 +1,5 @@
+# 1. Create a class for the functions. 
+# 2. Define the variables that we are going to use.
+# 3. Define the questions and answers input.
+# 4. Define the file it is going to be saved as.
+# 5. Make a variable storage for the class.
