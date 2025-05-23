9bb2674f73ed9eed5601f2ac821ae78de88bd863
ycz <yczaubhreydy@gmail.com>
2025-05-23 18:10:33 +0800

    self initialization

--- quiz_text_file_creator.py
+++ quiz_text_file_creator.py
@@ -1,5 +1,10 @@
 # 1. Create a class for the functions. 
+class QuizCreator:
 # 2. Define the variables that we are going to use.
+    def __init__(self):
+        self.title = ""
+        self.item_quantity = 0   #These are the relevant datas in my previous code. 
+        self.quiz_data = []
 # 3. Define the questions and answers input.
 # 4. Define the file it is going to be saved as.
 # 5. Make a variable storage for the class.
