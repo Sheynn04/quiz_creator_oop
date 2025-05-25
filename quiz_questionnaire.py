c9dbd419b9771dec4a8e940c9d63f20365aa9f43
ycz <yczaubhreydy@gmail.com>
2025-05-25 21:27:38 +0800

    pseudocode and imports

---
+++ quiz_questionnaire.py
@@ -0,0 +1,9 @@
+# 1. Import the needed library.
+import random
+
+# 2. Create a class for the question outputs.
+class Question:
+
+# 3. Define the variables we are going to include in this class.
+
+# 4. Define each variables we listed.
