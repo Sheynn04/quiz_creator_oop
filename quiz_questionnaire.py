b251f276f33361124788af9327f11bf1e9ba24fe
ycz <yczaubhreydy@gmail.com>
2025-05-25 21:29:54 +0800

    defining output variables

--- quiz_questionnaire.py
+++ quiz_questionnaire.py
@@ -5,5 +5,17 @@ import random
 class Question:
 
 # 3. Define the variables we are going to include in this class.
+    def __init__(self,questions,choices,answer):
+        self.questions = questions
+        self.choices = choices
+        self.answer = answer
+
+    def output(self):
+        print(f"Q{ques_num +1}: {ques_contents['question']}")
+        print(f"A. {ques_contents['choices'][0]}")
+        print(f"B. {ques_contents['choices'][1]}")
+        print(f"C. {ques_contents['choices'][2]}")
+        print(f"D. {ques_contents['choices'][3]}")
+    
 
 # 4. Define each variables we listed.
